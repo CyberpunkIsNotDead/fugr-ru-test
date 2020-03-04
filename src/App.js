b626f265c59fcb0820a9db79a74f33b6f0e328fd
neuro <asuka2ndchild@gmail.com>
2020-03-04 17:33:29 +0300

    delete unneeded strings

--- src/App.js
+++ src/App.js
@@ -2,19 +2,17 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { MainPage } from './pages/MainPage';
 import { DataContextWrapper } from './Context/DataContextWrapper';
-import { DataFilterContextWrapper } from './Context/DataFilterContextWrapper';
+
 
 function App() {
   return (
     <DataContextWrapper>
-      <DataFilterContextWrapper>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path='/' component={MainPage} />
-            <Route path='/page/:page' component={MainPage} />
-          </Switch>
-        </BrowserRouter>
-      </DataFilterContextWrapper>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={MainPage} />
+          <Route path='/page/:page' component={MainPage} />
+        </Switch>
+      </BrowserRouter>
     </DataContextWrapper>
   );
 }
